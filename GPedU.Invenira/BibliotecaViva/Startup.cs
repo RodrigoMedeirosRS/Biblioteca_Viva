133818008ae3a7b8761223df7bc24b054db7f883
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2020-05-13 20:35:23 -0300

    feat(ajustes connections string)

--- GPedU.Invenira/BibliotecaViva/Startup.cs
+++ GPedU.Invenira/BibliotecaViva/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+using BibliotecaViva.Models.DTO;
 
 namespace BibliotecaViva
 {
@@ -26,6 +29,10 @@ namespace BibliotecaViva
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEntityFrameworkNpgsql().AddDbContext<BibliotecaVivaContext>(opt=>
+            opt.UseNpgsql(Configuration.GetConnectionString("BiliotecaVivaConnectionString")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
