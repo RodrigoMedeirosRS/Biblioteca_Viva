233f97096c43d3f12bdbb5791d399199c4b5eec3
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2020-07-15 08:51:25 -0300

    feat(swagger)
    Adicionado swagger ao projeto

--- CodigoFonte/GPedU/BibliotecaViva/Startup.cs
+++ CodigoFonte/GPedU/BibliotecaViva/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.OpenApi.Models;
+using BibliotecaViva.Models.DTO;
+using BibliotecaViva.Controllers;
 
 namespace BibliotecaViva
 {
@@ -26,11 +24,33 @@ namespace BibliotecaViva
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEntityFrameworkNpgsql().AddDbContext<biblioteca_vivaContext>(opt =>
+
+            services.AddTransient(typeof(PessoaController))
+
+            );
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Biblioteca Viva");
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -46,6 +66,6 @@ namespace BibliotecaViva
             {
                 endpoints.MapControllers();
             });
-        }
+        } 
     }
 }
