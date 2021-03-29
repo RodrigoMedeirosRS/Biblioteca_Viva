40bdc380db7baa56705c582b68aa94f429a70fa7
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2021-03-29 00:16:10 -0300

    feat(Refactor)
    implementada consulta e cadastro pessoa.

--- CodigoFonte/GPedU/BibliotecaViva/DTO/BaseDTO.cs
+++ CodigoFonte/GPedU/BibliotecaViva/DTO/BaseDTO.cs
@@ -2,24 +2,6 @@ namespace BibliotecaViva.DTO
 {
     public abstract class BaseDTO
     {
-        public BaseDTO()
-        {
-            Codigo = null;
-        }
-        public BaseDTO(int? codigo)
-        {
-            Codigo = codigo;
-        }
-        public void AtualizarCodigo(int? codigo)
-        {
-            Codigo = Codigo ?? codigo;
-        }
-
-        public void DefinirCodigo(int? codigo)
-        {
-            Codigo = codigo;
-        }
-
-        public int? Codigo { get; protected set; }
+        public int? Codigo { get; set; }
     }
 }
